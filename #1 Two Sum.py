05f2849e9b3a42b8a12aa4e04d5a863d45205347
Katherine T <55773448+Wen0420@users.noreply.github.com>
2020-11-05 17:37:38 +1100

    Added len() rang()

--- #1 Two Sum.py
+++ #1 Two Sum.py
@@ -27,3 +27,69 @@ class Solution(object):
             else:
                 return [dict[target-nums[i]], i]
 
+#语法注释
+len()
+'''
+1：作用：返回字符串、列表、字典、元组等长度
+
+2：语法：len(str)
+
+3：参数：
+str：要计算的字符串、列表、字典、元组等
+
+4：返回值：字符串、列表、字典、元组等元素的长度
+
+5：实例
+'''
+#5.1、计算字符串的长度：
+s = "hello good boy doiido"
+print(len(s)) #21
+
+#5.2、计算列表的元素个数：
+l = ['h','e','l','l','o']
+print(len(l)) #5
+
+#5.3、计算字典的总长度(即键值对总数)：
+d = {'num':123,'name':"doiido"}
+print(len(d)) #2
+
+#5.4、计算元组元素个数：
+t = ('G','o','o','d')
+print(len(t)) #4
+
+range()
+'''
+python内置range()函数的作用是什么？它能返回一系列连续增加的整数，
+它的工作方式类似于分片，可以生成一个列表对象。range函数大多数时常出现在for循环中，
+在for循环中可做为索引使用。其实它也可以出现在任何需要整数列表的环境中，
+在python 3.0中range函数是一个迭代器。
+
+range (stop)
+range (start,stop[ ,step])
+
+start：计数从start开始，默认是从0开始。例如，range(5) 就相当于是range(0,5)
+
+stop : 计数到stop结束，但不包括stop。例如:range(0,5)是 [0,1,2,3,4]
+
+step:步长，默认为1，例如，range(0,5)等价于range(0,5,1)
+
+for i in range ()作用：
+
+range()是一个函数， for i in range () 就是给i赋值：
+比如 for i in range （1，3）：
+就是把1,2依次赋值给i
+
+'''
+
+#range()函数内只有一个参数，则表示会产生从0开始计数的整数列表：
+i = range(4)
+print(i)
+
+for i in range(4):
+    print(i)
+
+for i in range(0,30,5):
+    print(i)
+
+c = list(range(0,30,5))
+print(c)
