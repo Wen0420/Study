fd66917e0eb64e5a231676d5e4b03f7371ce684e
K.T <55773448+Wen0420@users.noreply.github.com>
2020-11-18 19:35:35 +1100

    added to 007,homework added to 006

--- 小甲鱼.py
+++ 小甲鱼.py
@@ -199,3 +199,140 @@ i = int(input("请输入一个整数："))
 while i > 0:
     print(' ' * (i - 1) + '*' * i)
     i = i - 1
+
+#7常用操作符
+a = b = c = d = 10
+a += 1
+b -= 3
+c *= 10
+d /= 8
+print(a) #11
+print(b) #7
+print(c) #100
+print(d) #1.25
+
+print(10//8) #1
+print(5%2) #1
+print(3**2) #9
+print(-3*2 + 5/-2 -4)#-12.5
+print(-3**2)#-9
+print(3**-2)#0.1111111111111111
+
+#005 homework 动动手 0
+import random
+
+times = 3
+secret = random.randint(1, 10)
+
+print('------------------我爱鱼C工作室------------------')
+guess = 0
+print("不妨猜一下小甲鱼现在心里想的是哪个数字：", end=" ")
+
+while (guess != secret) and (times > 0):
+    temp = input()
+
+    if temp.isdigit():
+        guess = int(temp)
+        if guess == secret:
+            print("我草，你是小甲鱼心里的蛔虫吗？！")
+            print("哼，猜中了也没有奖励！")
+        else:
+            if guess > secret:
+                print("哥，大了大了~~~")
+            else:
+                print("嘿，小了，小了~~~")
+            if times > 1:
+                print("再试一次吧：", end='')
+            else:
+                print("机会用光咯T_T")
+    else:
+        print("抱歉，您的输入有误，请输入一个整数：", end='')
+
+    times = times - 1  # 用户每输入一次，可用机会就-1
+
+print("游戏结束，不玩啦^_^")
+
+#005 homework 动动手 1. 写一个程序，判断给定年份是否为闰年。
+temp = input('请输入一个年份：')
+while not temp.isdigit():
+    temp = input("抱歉，您的输入有误，请输入一个整数：")
+
+year = int(temp)
+if year/400 == int(year/400):
+    print(temp + ' 是闰年！')
+else:
+    if (year/4 == int(year/4)) and (year/100 != int(year/100)):
+        print(temp + ' 是闰年！')
+    else:
+        print(temp + ' 不是闰年！')
+
+#006 homework 0. 请写一个程序打印出 0~100 所有的奇数。
+i = 0
+while i <= 100:
+    if i % 2 != 0:
+        print(i, end=' ')
+        i += 1
+    else:
+        i += 1
+
+#2. 题目：请编程求解该阶梯至少有多少阶？
+'''
+爱因斯坦曾出过这样一道有趣的数学题：有一个长阶梯，若每步上2阶，最后剩1阶；若每步上3阶，最后剩2阶；若每步上5阶，最后剩4阶；若每步上6阶，最后剩5阶；只有每步上7阶，最后刚好一阶也不剩。
+（小甲鱼温馨提示：步子太大真的容易扯着蛋~~~）
+
+题目：请编程求解该阶梯至少有多少阶？
+'''
+x = 7
+i = 1
+flag = 0
+
+while i <= 100:
+    if (x%2 == 1) and (x%3 == 2) and (x%5 == 4) and (x%6==5):
+        flag = 1
+    else:
+        x = 7 * (i+1) # 根据题意，x一定是7的整数倍，所以每次乘以7
+    i += 1
+
+if flag == 1:
+    print('阶梯数是：', x)
+else:
+    print('在程序限定的范围内找不到答案！')
+#这里，flag 的作用：判断是否满足条件，退出循环
+    #动动手2这样写简单多了
+    #第一种
+    a = 1
+    while not ((a % 3 == 2) and (a % 5 == 4) and (a % 6 == 5) and (a % 7 == 0) and (a % 2 == 1)):
+        a += 1
+    print(a)
+    #动动手2
+    #第2种 不确定
+# x = 7
+# i = 1
+# while i<= 100:
+#     if (x%2 == 1) and (x%3 == 2 ) and (x%5 == 4) and (x%6 == 5) and (x%7 == 0):
+# print('爱因斯坦所求阶梯为;',x)
+# x = 7 * (i+1)
+# i += 1
+# else:
+#     x = 7 * (i+1)
+#     i += 1
+# 循环100次，结果为：
+# 爱因斯坦所求阶梯为; 119
+# 爱因斯坦所求阶梯为; 329
+# 爱因斯坦所求阶梯为; 539
+
+#如果程序为：
+# x = 7
+# i = 1
+# while i<= 100:
+#     if (x%2 == 1) and (x%3 == 2 ) and (x%5 == 4) and (x%6 == 5) and (x%7 == 0):
+# print('爱因斯坦所求阶梯为;',x)
+#     else:
+#     x = 7 * (i+1)
+#     i += 1
+
+# 结果为：输出无限多个“ 爱因斯坦所求阶梯为; 119 ”
+# 原因是：i值被固定为一个大于零的正整数，故而无限循环，解决办法为↓
+# print('爱因斯坦所求阶梯为;',x)
+# i=0
+
