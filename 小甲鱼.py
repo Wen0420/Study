ad5d459aad9b006b6f756743604756fb5f250d2d
K.T <55773448+Wen0420@users.noreply.github.com>
2020-11-29 14:24:44 +1100

    added no

--- 小甲鱼.py
+++ 小甲鱼.py
@@ -1670,6 +1670,21 @@ def sum(x):
 print(sum([1, 2.1, 2.3, 'a', '1', True]))
 
 
+####
+编写一个进制转换程序
+
+q = True
+while q:
+    num = input('请输入一个整数(输入Q结束程序)：')
+    if num != 'Q':
+        num = int(num)
+        print('十进制 -> 十六进制 : %d -> 0x%x' % (num, num))
+        print('十进制 -> 八进制 : %d -> 0o%o' % (num, num))
+        print('十进制 -> 二进制 : %d -> ' % num, bin(num))
+    else:
+        q = False
+
+
 
 
 
