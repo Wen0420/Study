e46f0d0b4b04ef9753c158335ef656b2be9fe23c
K.T <55773448+Wen0420@users.noreply.github.com>
2020-12-03 23:33:27 +1100

    added pros and cons of python

--- little Turtle first part review.py
+++ little Turtle first part review.py
@@ -1,1 +1,19 @@
 #IDLE is Python's Integrated Development and Learning Environment.
+
+#What can I do with python?
+'''
+https://realpython.com/what-can-i-do-with-python/
+From web development to data science, machine learning, and more, Python’s real-world applications are limitless. However,As an interpreted language, Python has trouble interacting with low-level devices, like device drivers. For instance, you’d have a problem if you wanted to write an operating system with Python only. You’re better off sticking with C or C++ for low-level applications.
+Pros:
+#1: Automate the Boring Stuff
+#2: Stay on Top of Bitcoin Prices
+#3: Create a Calculator
+#4: Mine Twitter Data
+#5: Build a Microblog With Flask
+#6: Build a Blockchain
+#7: Bottle Up a Twitter Feed
+#8: Play PyGames
+#9: Choose Your Own Adventure
+#10: Say “Hello World!” to Machine Learning
+#11: Get Challenged
+'''
